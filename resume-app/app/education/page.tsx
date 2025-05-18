000426fe9cb9433ac2aec90baf4f732df26812dd
MohannadMilhem <mohannadmilhem278@gmail.com>
2025-05-18 09:50:10 +0300

    footer is done

--- resume-app/app/education/page.tsx
+++ resume-app/app/education/page.tsx
@@ -1,3 +1,16 @@
 export default function Education() {
-  return <div></div>;
+  return (
+    <div className="md:text-3xl pt-10 pl-35 font-bold min-h-screen flex flex-col space-y-20">
+      Education
+      <div className="md:text-2xl text-xl pt-5 pl-10 max-w-200">
+        {" "}
+        Acquired 90 academic points at the Technion, while i learned the most
+        important courses, such as Algorithims, Data structre, OOP, Operating
+        systems, Matam, Atam, Intro to computer science, programming languages
+      </div>
+      <div className="md:text-2xl text-xl pt-5 pl-10 max-w-200">
+        Great self learner, studied the web development all by myself,
+      </div>
+    </div>
+  );
 }
